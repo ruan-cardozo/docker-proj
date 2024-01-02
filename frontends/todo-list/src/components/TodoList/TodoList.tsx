4f0fa490e79d9dffa14a2e535805ab13c46030ac
ruan-cardozo <ruanvcardozo@gmail.com>
2024-01-02 11:06:58 -0300

    (refactor) new todolist

--- frontends/todo-list/src/components/TodoList/TodoList.tsx
+++ frontends/todo-list/src/components/TodoList/TodoList.tsx
@@ -2,42 +2,56 @@ import { useState } from "react";
 import { TaskList, TaskListItem } from "./types";
 
 const initialTasks: TaskList = [
-    { id: 1, name: "Levar o cachorro para passear", isDone: true, isEditing: false },
-    { id: 4, name: "Ir ao mercado e comprar XYZ", isDone: true, isEditing: false},
-    { id: 2, name: "Levar o lixo para fora", isDone: true, isEditing: false },
-    { id: 3, name: "Lavar louça", isDone: true, isEditing: false}
+    { id: 1, name: "Levar o cachorro para passear", isDone: true, isEditing: true },
+    { id: 2, name: "Levar o lixo para fora", isDone: true, isEditing: true },
+    { id: 3, name: "Lavar louça", isDone: true, isEditing: true}
 ];
 
 function TodoList() {   
     const [tasks, setTasks] = useState<TaskList>(initialTasks); 
+
     return (
+
     <div>
-        <button type="button" onClick={() => {
+        <button style={{cursor: "pointer"}} type="button" onClick={() => {
+
             const newItem: TaskListItem = {
                 id: tasks.length + 1,
-                name: "New Task",
+                name: "",
                 isDone: false,
                 isEditing: true
             };
+
             tasks.push(newItem);
             setTasks([...tasks]);
         }}>Add</button>    
         <ul style={{ listStyleType: "counter-style"}}>
             {tasks.map((task,index) => (
                 <li key={task.id} style={{textAlign: "left"}}>
-                    <input type="checkbox" checked={task.isDone} onChange={() => {
+                    <input style={{cursor: "pointer"}} type="checkbox" checked={task.isDone} onChange={() => {
                         task.isDone = !task.isDone;
                         setTasks([...tasks]);
                     }}></input>
-                    <input type="text" onChange={(e) => {
-                        console.log(e);
-                        task.name = e.target.value
-                        setTasks([...tasks]);
-                    }} value={task.name}/>
-                    <button type="button" onClick={() => {  
+                    {task.isEditing ? (
+                        <input style={{cursor: "pointer"}} type="text" placeholder="Write here..." onChange={(e) => {
+                            task.name = e.target.value;
+                            setTasks([...tasks]);
+                        }} value={task.name}/>
+                        ) : (
+                        <span style={{textDecoration: task.isDone ? 'line-through' : 'none'}}>{task.name}</span>
+                    )}
+                        <button style={{cursor: "pointer", marginLeft: '5px', alignItems: 'center'}} type="button" onClick={() => {
+                            task.isEditing = true;  
+                            setTasks([...tasks]);
+                    }}>Edit</button>
+                    <button style={{cursor: "pointer", marginLeft: '5px', alignItems: 'center'}} type="button" onClick={() => {  
                         tasks.splice(index,1); 
                         setTasks([...tasks]); 
                     }}>Del</button> 
+                    <button style={{cursor: "pointer", marginLeft: '5px', alignItems: 'center'}} type="button" onClick={() => {
+                        task.isEditing = false;
+                        setTasks([...tasks]);
+                    }}>Save</button>
                 </li>
             ))}
         </ul>
