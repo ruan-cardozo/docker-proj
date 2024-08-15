a7a7987a8e7e59e5eb960b2e390a9843f1de30c1
ruan-cardozo <ruanvcardozo@gmail.com>
2024-08-14 22:09:14 -0300

    adjusting code

--- backend/python-backend-example/django-proj/bin/activate.csh
+++ backend/python-backend-example/django-proj/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/ruan/workspace/study/docker-proj/backend/pyhton-backend-example/python_env"
+setenv VIRTUAL_ENV "/home/ruan/workspace/study/docker-proj/backend/python-backend-example/django-proj"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -18,8 +18,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(python_env) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(python_env) "
+    set prompt = "(django-proj) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(django-proj) "
 endif
 
 alias pydoc python -m pydoc
--- backend/python-backend-example/django-proj/bin/activate.fish
+++ backend/python-backend-example/django-proj/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ruan/workspace/study/docker-proj/backend/pyhton-backend-example/python_env"
+set -gx VIRTUAL_ENV "/home/ruan/workspace/study/docker-proj/backend/python-backend-example/django-proj"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -56,7 +56,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) "(python_env) " (set_color normal)
+        printf "%s%s%s" (set_color 4B8BBE) "(django-proj) " (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
@@ -65,5 +65,5 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(python_env) "
+    set -gx VIRTUAL_ENV_PROMPT "(django-proj) "
 end
